31ceb3b0169b4bbab4b8a2baa45d225d24b59538
Alinor <havengar@hotmail.com.br>
2026-03-13 16:50:23 -0300

    Add implementations for exercises Uni3Exe15 and Uni3Exe16, and create .gitignore files for Unidade_3

--- src/Unidade_3/Uni3Exe15.java
+++ src/Unidade_3/Uni3Exe15.java
@@ -1,5 +1,41 @@
 package Unidade_3;
 
-public class Uni3Exe15 {
+import java.util.Scanner;
 
+public class Uni3Exe15 {
+    private int centena;
+    private int dezena;
+    private int unidade;
+    private int numero;
+
+    public Uni3Exe15(int numero){
+        this.numero = numero;
+    }
+
+    public int calcCentena(int numero){
+        return centena = (numero / 100);
+    }
+    public int calcDezena(int numero){
+        return dezena = (numero % 100) / 10;
+    }
+    public int calcUnidade(int numero){
+        return unidade = ((numero % 100) % 10) / 1;
+    }
+
+    public static void main(String[] args) {
+        Uni3Exe15 classExec = new Uni3Exe15(0);
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.print("Digite um numero de 3 digitos:");
+            int numero = scan.nextInt();
+            if (numero <= 999){
+                int centena = classExec.calcCentena(numero);
+                int dezena = classExec.calcDezena(numero);
+                int unidade = classExec.calcUnidade(numero);
+                System.out.printf("%s centena(s) %s dezena(s) %s unidade(s)%n", centena, dezena, unidade);
+                scan.close();
+                break;
+            }
+        }
+    }
 }
--- src/Unidade_3/Uni3Exe16.java
+++ src/Unidade_3/Uni3Exe16.java
@@ -1,5 +1,44 @@
 package Unidade_3;
 
 public class Uni3Exe16 {
+    private float valorCompra;
+    private float valorCliente;
 
+    public Uni3Exe16(float valorCompra, float valorCliente){
+        this.valorCompra = valorCompra;
+        this.valorCliente = valorCliente;
+    }
+
+    public float valorTroco(float valorCompra, float valorCliente){
+        float result = (valorCliente - valorCompra);
+        return result;
+    }
+
+    public float notasTroco100(float result){
+        return result = (valorTroco(valorCompra, valorCliente) / 100);
+    }
+    public float notasTroco10(float result){
+        return result = (valorTroco(valorCompra, valorCliente) / 10);
+    }
+    public float notasTroco1(float result){
+        return result = (valorTroco(valorCompra, valorCliente) % 1);
+    }
+
+    public static void main(String[] args) {
+        Uni3Exe16 classExec = new Uni3Exe16(0, 0);
+        float result = classExec.valorTroco(1258, 1300);
+        System.out.println(result);
+        if(classExec.notasTroco100(result) >= 0 ){
+            result = classExec.notasTroco100(result);
+            System.out.println(result);
+        }
+        if(classExec.notasTroco10(result) >= 0 ){
+            result = classExec.notasTroco10(result);
+            System.out.println(result);
+        }
+        if(classExec.notasTroco1(result) >= 0 ){
+            result = classExec.notasTroco1(result);
+            System.out.println(result);
+        }
+    }
 }
