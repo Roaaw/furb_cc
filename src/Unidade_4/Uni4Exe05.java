d1dcf42f309b8e526fcd50223341ceac6ed990bb
Alinor <havengar@hotmail.com.br>
2026-03-23 15:59:58 -0300

    Remove Uni2Exe19 class and implement Uni4Exe05 with color check functionality

--- src/Unidade_4/Uni4Exe05.java
+++ src/Unidade_4/Uni4Exe05.java
@@ -1,8 +1,40 @@
 ﻿package Unidade_4;
 
+import java.util.Scanner;
+
 public class Uni4Exe05 {
+    private String cor; 
+
+    public Uni4Exe05(String cor){
+        this.cor = cor;
+    }
+
+    public String isAzul(){
+        String isAzul;
+        if(cor.equalsIgnoreCase("Sim")){
+            isAzul = "Sim";
+        }else{
+            isAzul = "Não";
+        }
+        return isAzul;
+    }
     public static void main(String[] args) {
-        System.out.println("Uni4Exe05 - placeholder");
-        // TODO: implementar exercício 5 da unidade 4
+        Scanner sc = new Scanner(System.in);
+        System.out.println("A cor é azul?");
+        String cor = sc.next();
+        sc.close();
+        Uni4Exe05 classExec = new Uni4Exe05(cor);
+        System.out.println(classExec.isAzul());
     }
 }
+
+/*
+TESTE
+A cor é azul?
+sim
+Sim
+
+A cor é azul?
+nao
+Não
+*/
