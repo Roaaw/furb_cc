37340f0faf4e10a462378f6898c1b10878562be2
Alinor <havengar@hotmail.com.br>
2026-03-27 03:41:16 -0300

    Refactor Uni4Exe25: implement basic arithmetic operations and user input handling

--- src/Unidade_4/Uni4Exe25.java
+++ src/Unidade_4/Uni4Exe25.java
@@ -1,8 +1,47 @@
 ﻿package Unidade_4;
 
+import java.util.Scanner;
+
 public class Uni4Exe25 {
+    Scanner sc = new Scanner(System.in);
+    private int numero1, numero2, opcao;
+
+    public Uni4Exe25(){
+        entrada();
+        saida();
+    }
+    private void entrada(){
+        System.out.print("Informe dois números: ");
+        numero1 = sc.nextInt();
+        numero2 = sc.nextInt();
+        System.out.print("1-Soma\n"+"2-Diferença\n"+"3-Produto\n"+"4-Divisão\n"+"Escolha uma opção: ");
+        opcao = sc.nextInt();
+    }
+    private void saida(){
+        switch (opcao) {
+            case 1:
+                System.out.print(numero1 + numero2);
+                break;
+            case 2:
+                System.out.print(numero1 - numero2);
+                break;
+            case 3:
+                System.out.print(numero1 * numero2);
+                break;
+            
+            case 4:
+                if(numero2 != 0){
+                    System.out.print(numero1 / numero2);
+                }else{
+                    System.out.print("O denominador não pode ser 0");
+                }
+                    break;
+                
+            default:
+                break;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Uni4Exe25 - placeholder");
-        // TODO: implementar exercício 25 da unidade 4
+        new Uni4Exe25();
     }
 }
