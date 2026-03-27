642c08b4492a6b11b699d9de4bfaa50d4cdc5a50
Alinor <havengar@hotmail.com.br>
2026-03-27 02:42:32 -0300

    Refactor Uni4Exe22: implement course selection logic and output results

--- src/Unidade_4/Uni4Exe22.java
+++ src/Unidade_4/Uni4Exe22.java
@@ -1,8 +1,60 @@
 ﻿package Unidade_4;
 
+import java.util.Scanner;
+
 public class Uni4Exe22 {
+    Scanner sc = new Scanner(System.in);
+    private int escolha;
+
+    public Uni4Exe22(){
+        entrada();
+        saida();
+    }
+    private void entrada(){
+        System.out.printf("1 - Ciência da Computação \n"+"2 - Licenciatura da Computação \n"+"3 - Sistemas de Informação \n"+"Informe o numero do seu curso: ");
+        escolha = sc.nextInt();
+    }
+    private void saida(){
+        switch (escolha) {
+            case 1:
+                System.out.println("Bacharel em Ciência da Computação");
+                break;
+            case 2:
+                System.out.println("Licenciado em Computação");
+                break;
+            case 3:
+                System.out.println("Bacharel em Sistemas de Informação");
+                break;
+        
+            default:
+
+                break;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Uni4Exe22 - placeholder");
-        // TODO: implementar exercício 22 da unidade 4
+        new Uni4Exe22();
     }
 }
+/*
+TESTE 1
+
+1 - Ciência da Computação 
+2 - Licenciatura da Computação
+3 - Sistemas de Informação
+Informe o numero do seu curso: 1
+Bacharel em Ciência da Computação
+TESTE 2
+
+1 - Ciência da Computação 
+2 - Licenciatura da Computação
+3 - Sistemas de Informação
+Informe o numero do seu curso: 2
+Licenciado em Computação
+TESTE 3
+
+1 - Ciência da Computação 
+2 - Licenciatura da Computação
+3 - Sistemas de Informação
+Informe o numero do seu curso: 3
+Bacharel em Sistemas de Informação
+*/
