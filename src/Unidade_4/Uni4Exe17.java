9c950f8e92f9aa59377ce7dd917b4ed05c7108ca
Alinor <havengar@hotmail.com.br>
2026-03-26 21:50:26 -0300

    Refactor Uni4Exe17: implement input handling and logic for income tax calculation

--- src/Unidade_4/Uni4Exe17.java
+++ src/Unidade_4/Uni4Exe17.java
@@ -1,8 +1,41 @@
 ﻿package Unidade_4;
 
+import java.util.Scanner;
+
 public class Uni4Exe17 {
+    Scanner sc = new Scanner(System.in);
+    private float rendaAnual, numDependentes, rendaAnualLiquida;
+    private final float descDependentes = 0.02f;
+
+    public Uni4Exe17(){
+        entrada();
+        logica();
+        saida();
+    }
+
+    private void entrada(){
+        System.out.print("Informe a renda anual: ");
+        rendaAnual = sc.nextFloat();
+        System.out.print("Informe a quantidade de denpendentes: ");
+        numDependentes = sc.nextFloat();
+    }
+    private void logica(){
+        if(rendaAnual <= 2000f){
+
+        }
+        if(rendaAnual > 2000f && rendaAnual <= 5000f){
+
+        }else if(rendaAnual > 5000f && rendaAnual <= 10000f){
+
+        }else{
+            
+        }
+
+    }
+    private void saida(){
+        System.out.print("O imposto é de %s: %.2f",);
+    }
     public static void main(String[] args) {
-        System.out.println("Uni4Exe17 - placeholder");
-        // TODO: implementar exercício 17 da unidade 4
+
     }
 }
