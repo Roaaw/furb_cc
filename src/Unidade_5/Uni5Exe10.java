bf36420d2acc289450a909e21e7bbb74ae4d7650
Alinor <havengar@hotmail.com.br>
2026-04-30 20:25:47 -0300

    Add Uni5Exe10 class to calculate and print the squares of numbers based on a specific formula

---
+++ src/Unidade_5/Uni5Exe10.java
@@ -0,0 +1,11 @@
+package Unidade_5;
+
+public class Uni5Exe10 {
+    public static void main(String[] args) {
+        for(int i=1; i <=10; i++){
+            int num = (i*10 + 25);
+            int r = (int)Math.pow(num, 2);
+            System.out.println(r);
+        }
+    }
+}
