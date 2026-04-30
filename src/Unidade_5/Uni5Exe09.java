e00be2792ec2f06f73d9efa7f610fe7e8e167d86
Alinor <havengar@hotmail.com.br>
2026-04-30 16:55:40 -0300

    Add Uni5Exe09 class to collect student names and count those aged 20 and above

---
+++ src/Unidade_5/Uni5Exe09.java
@@ -0,0 +1,31 @@
+package Unidade_5;
+
+import java.util.Scanner;
+
+public class Uni5Exe09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int vinteM = 0;
+        String listaNome = "";
+        for(int i=0; i<n; i++){
+            System.out.print("Digite o nome do aluno: ");
+            String nome = sc.next();
+            System.out.print("Digite a idade do aluno: ");
+            int idade = sc.nextInt();
+            if(idade == 18){
+                if(listaNome == ""){
+                    listaNome += nome;
+                }else{
+                    listaNome += " e "+nome;
+                }
+            }
+            if(idade >= 20){
+                vinteM++;
+            }
+        }
+        System.out.println("Nomes dos alunos que tem 18 anos: "+listaNome);
+        System.out.println("Quantidade de alunos que tem idade acima de 20 anos: "+vinteM);
+        sc.close();
+    }
+}
