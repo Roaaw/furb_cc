298db855484e4c4bb727dc298b335a1cdec1c732
Alinor <havengar@hotmail.com.br>
2026-04-30 20:53:55 -0300

    Change variable types from float to double in Uni5Exe03 for improved precision and format output to two decimal places

--- src/Unidade_5/Uni5Exe03.java
+++ src/Unidade_5/Uni5Exe03.java
@@ -2,13 +2,13 @@ package Unidade_5;
 
 public class Uni5Exe03 {
     public static void main(String[] args) {
-    float num = 1;
-    float div;
-    float result = 0;
+    double num = 1;
+    double div;
+    double result = 0;
         for(int i=1; i<100; i++){
             div = num / i;
             result += div;
         }
-        System.out.println(result);
+        System.out.printf("%.2f",result);
     }
 }
