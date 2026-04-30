7b2353ec0548c9cd155c0c6ad78e732a8d7d5795
Alinor <havengar@hotmail.com.br>
2026-04-30 20:49:50 -0300

    Refactor Uni5Exe02 to initialize totalPar and totalImpar variables and improve code readability

--- src/Unidade_5/Uni5Exe02.java
+++ src/Unidade_5/Uni5Exe02.java
@@ -2,10 +2,16 @@ package Unidade_5;
 
 public class Uni5Exe02 {
     public static void main(String[] args) {
+        int totalPar = 0;
+        int totalImpar = 0;
         for(int i=1; i<=100; i++){
             if(i % 2 == 0){
-                
+                totalPar += i; 
+            }else{
+                totalImpar += i;
             }
         }
+        System.out.println("Total Par: "+totalPar);
+        System.out.println("Total Impar: "+totalImpar);
     }
 }
