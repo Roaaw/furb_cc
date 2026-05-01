e26f635027d1e5ade6c7b3777478afa451568465
Alinor <havengar@hotmail.com.br>
2026-04-30 21:18:05 -0300

    Refactor Uni5Exe05 to accept user input for repetitions and improve loop logic; add Uni5Exe12 to implement a pattern printing based on user input

--- src/Unidade_5/Uni5Exe05.java
+++ src/Unidade_5/Uni5Exe05.java
@@ -1,10 +1,15 @@
 package Unidade_5;
 
+import java.util.Scanner;
+
 public class Uni5Exe05 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int r = 0;
         int r2 = 4;
-        for(int i=0; i<=10; i++){
+        System.out.print("Informe o número de repetições: ");
+        int n = sc.nextInt();
+        for(int i=0; i<=n; i++){
             if(i % 2 == 0){
                 r = r2 *2;
                 r2 = r;
@@ -13,5 +18,6 @@ public class Uni5Exe05 {
             }
             System.out.println(r);
         }
+        sc.close();
     }
 }
---
+++ src/Unidade_5/Uni5Exe12.java
@@ -0,0 +1,27 @@
+package Unidade_5;
+
+import java.util.Scanner;
+
+public class Uni5Exe12 {
+    public static void main(String[] args) {
+        int n = 0;
+        int t = 0;
+        int k = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número inteiro positivo:");
+        n = sc.nextInt();
+        if(n > 0){
+            for(int i=0; i<=n; i++){
+                t++;
+                for(int j=1; j<=t; j++){
+                    k++;
+                    System.out.print(k);
+                }
+                System.out.println();
+            }
+        }else{
+            System.out.println("Número informado não é positivo ou intiero.");
+        }
+        sc.close();
+    }
+}
