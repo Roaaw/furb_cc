31a7f47063b328780f3ed6d114a0fd5e20d78235
Alinor <havengar@hotmail.com.br>
2026-05-08 21:58:54 -0300

    Add Uni5Exe25: Implement basic game simulation with scoring

---
+++ src/Unidade_5/Uni5Exe25.java
@@ -0,0 +1,30 @@
+package Unidade_5;
+
+public class Uni5Exe25 {
+    public static void main(String[] args) {
+        int d = 0;
+        int e = 0;
+        String placar = "";
+        boolean jogo = true;
+        while(jogo){
+            placar = "         PLACAR\nEsquerda "+e+" - "+d+" Direita";
+            if((int)(Math.random() * (10 - 0) + 1) % 2 == 0){
+                d++;
+            }else{
+                e++;
+            }
+            placar = "         PLACAR\nEsquerda "+e+" - "+d+" Direita";
+            if(d >= 21 && d - e >= 2 || e >= 21 && e - d >= 2){
+                jogo = false;
+                if(d >= 21 && d - e >= 2){
+                    placar += "\n       Direita Ganhou";
+                }else if(e >= 21 && e - d >= 2){
+                    placar += "\n       Esquerda Ganhou";
+                }
+            }
+            // System.out.println(d - e);
+            System.out.println(placar);
+            // System.out.println(e - d);
+        }
+    }
+}
