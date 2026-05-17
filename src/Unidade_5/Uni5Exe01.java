1ad72a36553f8d7de767c81e52cfe1d74651f57a
Alinor <havengar@hotmail.com.br>
2026-05-17 06:07:34 -0300

    Refactor Uni5Exe01: Enhance number classification logic and streamline input/output handling

--- src/Unidade_5/Uni5Exe01.java
+++ src/Unidade_5/Uni5Exe01.java
@@ -3,15 +3,35 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe01 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for (int i=0; i<=20; i++){
-            
+    Scanner sc = new Scanner(System.in);
+    int input;
+    String output;
+    int count = 0;
+    public Uni5Exe01(){
+        logica();
+    }
+    private void entrada(){
+        System.out.print("Informe um número: ");
+        input = sc.nextInt();
+    }
+    private void logica(){
+        for(int i = 0; i<20; i++){
+            entrada();
+            count++;
             if(i % 2 == 0){
-                System.out.println(i+" Par");
-            }else
-                System.out.println(i+" Ímpar");
+                output = "Par";
+                saida();
+            }else{
+                output = "Ímpar";
+                saida();
+            }
         }
-        sc.close();
+        System.out.println(count);
+    }
+    private void saida(){
+        System.out.println(output);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe01();
     }
 }
