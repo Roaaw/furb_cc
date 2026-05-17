f434c6c67b33ef76a6a022b9d5bba38a93cb7d44
Alinor <havengar@hotmail.com.br>
2026-05-17 06:15:28 -0300

    Refactor Uni5Exe03: Move logic and output handling to separate methods for improved readability

--- src/Unidade_5/Uni5Exe03.java
+++ src/Unidade_5/Uni5Exe03.java
@@ -1,14 +1,25 @@
 package Unidade_5;
 
 public class Uni5Exe03 {
-    public static void main(String[] args) {
     double num = 1;
     double div;
     double result = 0;
+
+    public Uni5Exe03(){
+        logica();
+    }
+
+    private void logica(){
         for(int i=1; i<100; i++){
             div = num / i;
             result += div;
         }
-        System.out.printf("%.2f",result);
+        saida();
+    }
+    private void saida(){
+        System.out.printf("%.2f", result);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe03();
     }
 }
