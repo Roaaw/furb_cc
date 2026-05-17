f6b83652a9b74541ffc5509bbda227fbee5bb689
Alinor <havengar@hotmail.com.br>
2026-05-17 06:12:42 -0300

    Refactor Uni5Exe02: Move total calculation logic to instance methods and streamline main method

--- src/Unidade_5/Uni5Exe02.java
+++ src/Unidade_5/Uni5Exe02.java
@@ -1,9 +1,14 @@
 package Unidade_5;
 
 public class Uni5Exe02 {
-    public static void main(String[] args) {
-        int totalPar = 0;
-        int totalImpar = 0;
+    private int totalPar = 0;
+    private int totalImpar = 0;
+
+    public Uni5Exe02(){
+        logica();
+    }
+
+    private void logica(){
         for(int i=1; i<=100; i++){
             if(i % 2 == 0){
                 totalPar += i; 
@@ -11,7 +16,13 @@ public class Uni5Exe02 {
                 totalImpar += i;
             }
         }
+        saida();
+    }
+    private void saida(){
         System.out.println("Total Par: "+totalPar);
         System.out.println("Total Impar: "+totalImpar);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe02();
     }
 }
