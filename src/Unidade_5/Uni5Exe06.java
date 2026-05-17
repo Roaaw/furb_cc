c46cb6ccbeb1d94b55bf3fdd71703aa6848dd7ac
Alinor <havengar@hotmail.com.br>
2026-05-17 06:34:49 -0300

    Refactor Uni5Exe06: Move variable declarations to class level and streamline input and output handling

--- src/Unidade_5/Uni5Exe06.java
+++ src/Unidade_5/Uni5Exe06.java
@@ -1,20 +1,34 @@
 package Unidade_5;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni5Exe06 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float a;
-        float soma = 0;
-        float result = 0;
+    Scanner sc = new Scanner(System.in);
+    DecimalFormat df = new DecimalFormat("0.00");
+    private float a;
+    private float soma = 0;
+    private float result = 0;
+
+    public Uni5Exe06(){
+        logic();
+    }
+    private void input(){
+        System.out.print("Informe a altura: ");
+        a = sc.nextFloat();
+    }
+    private void logic(){
         for(int i=1; i<=20; i++){
-            System.out.print("Informe a altura: ");
-            a = sc.nextFloat();
+            input();
             soma += a;
             result = soma/i;
         }
-        System.out.println("A média das alturas é: "+result);
-        sc.close();
+        output();
+    }
+    private void output(){
+        System.out.println("A média das alturas é: "+df.format(result));
+    }
+    public static void main(String[] args) {
+        new Uni5Exe06();
     }
 }
