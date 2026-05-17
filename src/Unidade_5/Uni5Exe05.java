1cdb9d52d1537e01c1606696f8008225c511fe78
Alinor <havengar@hotmail.com.br>
2026-05-17 06:29:21 -0300

    Refactor Uni5Exe05: Move variable declarations to class level and streamline input and output handling

--- src/Unidade_5/Uni5Exe05.java
+++ src/Unidade_5/Uni5Exe05.java
@@ -3,13 +3,21 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe05 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int r = 0;
-        int r2 = 4;
+    Scanner sc = new Scanner(System.in);
+    private int r = 0;
+    private int r2 = 4;
+    private int n = 0;
+
+    public Uni5Exe05(){
+        logic();
+    }
+    private void input(){
         System.out.print("Informe o número de repetições: ");
-        int n = sc.nextInt();
-        for(int i=0; i<=n-1; i++){
+        n = sc.nextInt();
+    }
+    private void logic(){
+        input();
+        for(int i = 0; i <= n-1; i++){
             if(i % 2 == 0){
                 r = r2 *2;
                 r2 = r;
@@ -17,11 +25,21 @@ public class Uni5Exe05 {
                 r+=2;
             }
             if(i == n-1){
-                System.out.print(r);
+                n = 1;
+                output();
             }else{
-                System.out.print(r+",");
+                output();
             }
         }
-        sc.close();
+    }
+    private void output(){
+        if(n == 1){
+            System.out.print(r);
+        }else{
+            System.out.print(r+",");
+        }
+    }
+    public static void main(String[] args) {
+        new Uni5Exe05();
     }
 }
