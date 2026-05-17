cfa3d399050fdb4a5fce252297ed43b2548f154c
Alinor <havengar@hotmail.com.br>
2026-05-17 06:20:47 -0300

    Refactor Uni5Exe04: Move variable declarations to class level and streamline main method

--- src/Unidade_5/Uni5Exe04.java
+++ src/Unidade_5/Uni5Exe04.java
@@ -1,27 +1,34 @@
 package Unidade_5;
 
 public class Uni5Exe04 {
-    public static void main(String[] args) {
-        float num1 = 1;
-        float num2 = 2;
-        float tab = 0;
-        float j = 2;
-        float div = 0;
-        float result = 0;
-        for(int i=1; i<=20; i++){
+    private float num1 = 1;
+    private float num2 = 2;
+    private float tab = 0;
+    private float j = 2;
+    private float div = 0;
+    private float result = 0;
+
+    public Uni5Exe04(){
+        logica();
+    }
+    private void logica(){
+        for(int i = 1; i<=20; i++){
             num1 += 2;
             tab = i * j;
-            // System.out.println(num1);
             if(i == 1){
-                // System.out.println(num2);
                 div = num1 / num2;
             }else if(i >= 2){
                 num2 += tab;
-                // System.out.println(num2);
                 div = num1 / num2;
             }
             result += div;
-            System.out.println(result);
-        }
+            saida();
+        }  
+    }
+    private void saida(){
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe04();
     }
 }
