517ebd16803165af79e1b0f335c6b63b44c08e0c
Alinor <havengar@hotmail.com.br>
2026-05-19 11:49:00 -0300

    Refactor Uni5Exe10: Move sum variable to class level and streamline constructor and logic method

--- src/Unidade_5/Uni5Exe10.java
+++ src/Unidade_5/Uni5Exe10.java
@@ -1,8 +1,11 @@
 package Unidade_5;
 
 public class Uni5Exe10 {
-    public static void main(String[] args) {
-        int sum = 0;
+    private int sum = 0;
+    public Uni5Exe10(){
+        logic();
+    }
+    private void logic(){
         for(int i=0; i < 10000; i++){
             for(int j=0; j < 10000; j++){
                 int num = i+j;
@@ -18,5 +21,8 @@ public class Uni5Exe10 {
                 }
             }
         }
+    }
+    public static void main(String[] args) {
+        new Uni5Exe10();
     }
 }
