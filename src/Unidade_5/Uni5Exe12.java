54e852fec034f32f47073dec0a46580052ef4230
Alinor <havengar@hotmail.com.br>
2026-05-19 11:57:30 -0300

    Refactor Uni5Exe12: Move variable declarations to class level and streamline input/output handling

--- src/Unidade_5/Uni5Exe12.java
+++ src/Unidade_5/Uni5Exe12.java
@@ -3,29 +3,42 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe12 {
-    public static void main(String[] args) {
-        int n = 0;
-        int t = 0;
-        int k = 0;
-        Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
+    private int n = 0;
+    private int t = 0;
+    private int k = 0;
+    private String temp = "";
+    public Uni5Exe12(){
+        logic();
+    }
+    private void input(){
         System.out.print("Digite um número inteiro positivo:");
         n = sc.nextInt();
+    }
+    private void logic(){
+        input();
         if(n > 0){
             for(int i=0; i<=n; i++){
                 t++;
                 for(int j=1; j<=t; j++){
                     k++;
                     if(k < 10){
-                        System.out.print(k+"  ");
+                        temp += k+"  ";
                     }else{
-                        System.out.print(k+" ");
+                        temp += k+" ";
                     }
                 }
-                System.out.println();
+                temp += "\n";
             }
         }else{
-            System.out.println("Número informado não é positivo ou intiero.");
+            temp = "Número informado não é positivo ou intiero.";
         }
-        sc.close();
+        output();
+    }
+    private void output(){
+        System.out.println(temp);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe12();
     }
 }
