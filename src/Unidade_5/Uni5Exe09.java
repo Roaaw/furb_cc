bbda946850945904ffcdb155d7bfc227be25959c
Alinor <havengar@hotmail.com.br>
2026-05-19 11:42:10 -0300

    Refactor Uni5Exe09: Move variable declarations to class level and streamline input handling

--- src/Unidade_5/Uni5Exe09.java
+++ src/Unidade_5/Uni5Exe09.java
@@ -3,33 +3,56 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe09 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
+    private int n = 0;
+    private int vinteM = 0;
+    private int dezoitoM = 0;
+    private String listaNome = "";
+    private String nome = "";
+    private int idade = 0;
+
+    public Uni5Exe09(){
+        logic();
+    }
+    private void entrada_input(){
         System.out.print("Informe a quantidade de entradas: ");
-        int n = sc.nextInt();
-        int vinteM = 0;
-        String listaNome = "";
-        for(int i=0; i<n; i++){
-            System.out.print("Digite o nome do aluno: ");
-            String nome = sc.next();
-            sc.nextLine();
-            System.out.print("Digite a idade do aluno: ");
-            int idade = sc.nextInt();//Integer.parseInt(sc.nextLine());
+        n = sc.nextInt();
+    }
+    private void aluno_input(){
+        System.out.print("Digite o nome do aluno: ");
+        nome = sc.next();
+        sc.nextLine();
+        System.out.print("Digite a idade do aluno: ");
+        idade = sc.nextInt();//Integer.parseInt(sc.nextLine());
+    }
+    private void logic(){
+        entrada_input();
+        for(int i = 0; i < n; i++){
+            aluno_input();
             if(idade == 18){
+                dezoitoM++;
                 if(listaNome == ""){
                     listaNome += nome;
-                }else if(i == n-1){
-                    listaNome += " e "+nome;
-                }else{
-                    listaNome += ", "+nome;
+                }else if(listaNome != ""){
+                    listaNome += " "+nome;
                 }
             }
             if(idade >= 20){
                 vinteM++;
             }
         }
+        if(dezoitoM <= 2){
+            listaNome = listaNome.replace(" ", " e ");
+        }else if(dezoitoM > 2){
+            listaNome = listaNome.replace(" ", " , ");
+        }
+        output();
+    }
+    private void output(){
         System.out.println("Nomes dos alunos que tem 18 anos: "+listaNome);
         System.out.println("Quantidade de alunos que tem idade acima de 20 anos: "+vinteM);
-        sc.close();
+    }
+    public static void main(String[] args) {
+        new Uni5Exe09();
     }
 }
