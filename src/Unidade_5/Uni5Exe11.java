cae80c747be664fc98a9532590be5b7671bffdc4
Alinor <havengar@hotmail.com.br>
2026-05-19 11:51:27 -0300

    Refactor Uni5Exe11: Move variable b to class level and streamline logic and output methods

--- src/Unidade_5/Uni5Exe11.java
+++ src/Unidade_5/Uni5Exe11.java
@@ -1,8 +1,11 @@
 package Unidade_5;
 
 public class Uni5Exe11 {
-    public static void main(String[] args) {
-        int b = 0;
+    private int b = 0;
+    public Uni5Exe11(){
+        logic();
+    }
+    private void logic(){
         for(int i=1; i<=16; i++){
             if(i == 1){
                 b+=1;
@@ -11,8 +14,14 @@ public class Uni5Exe11 {
             }else if(i > 2){
                 b += b * 3;
             }
-            System.out.printf("Hora %s, Biscoito Quebrado: %s\n",i,b);
+            // System.out.printf("Hora %s, Biscoito Quebrado: %s\n",i,b);
         }
+        output();
+    }
+    private void output(){
         System.out.println("Número de biscoitos quebrados em 16 horas: "+b);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe11();
     }
 }
