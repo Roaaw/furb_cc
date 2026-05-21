d60e0d9af851e44b79a0292976c490a4f1f1a083
Alinor <havengar@hotmail.com.br>
2026-05-21 20:14:28 -0300

    .

--- src/Unidade_5/Uni5Exe15.java
+++ src/Unidade_5/Uni5Exe15.java
@@ -3,6 +3,38 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe15 {
+    private Scanner sc = new Scanner(System.in);
+    private float nota = 0;
+    private String nome = "";
+    private boolean exit = false;
+
+    public Uni5Exe15(){
+
+    }
+    private void entrada(){
+        if(nome == ""){
+            System.out.print("Nome do aluno: ");
+            nome = sc.next();
+        }
+        if(nota == 0){
+            System.out.print("Digite a nota: ");
+            nota = sc.nextFloat();
+        }else{
+            System.out.print("Digite a próxima nota: ");
+            nota += sc.nextFloat();
+        }
+    }
+    private void logica(){
+        while (!exit) {
+            if(nome.toLowerCase().equals("fim")){
+                exit = true;
+            }else{
+                for(int i=0; i <=1; i++){
+                    
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float nota = 0;
