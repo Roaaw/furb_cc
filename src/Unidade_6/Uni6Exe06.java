0ca6eb9af5f831ba72259c6f2bb16ef0fbda4ad2
Alinor <havengar@hotmail.com.br>
2026-05-21 21:48:22 -0300

    Add Uni6Exe06: Implement user input handling and value checking in array

--- src/Unidade_6/Uni6Exe06.java
+++ src/Unidade_6/Uni6Exe06.java
@@ -1,7 +1,41 @@
 package Unidade_6;
 
+import java.util.Scanner;
+
 public class Uni6Exe06 {
+    private Scanner sc = new Scanner(System.in);
+    private int tamanho;
+    private double[][] vetor = new double[2][5];
+
+    public Uni6Exe06(){
+
+    }
+    private void entrada(){
+        System.out.print("Informe o tamanho : ");
+        tamanho = sc.nextInt();
+    }
+    private void popularVetor(){
+        for(int i = 0; i < tamanho; i++){
+            System.out.printf("Informe um valor real: (%s)\n",i);
+            vetor[0][i] = sc.nextDouble();
+        }
+    }
+    private void checarValor(){
+        System.out.print("Digite um valor para checar se existe dentro do vetor: ");
+        vetor[1][0] = sc.nextDouble();
+        for(int i = 0; i < tamanho; i++){
+            if(vetor[1][0] == vetor[0][i]){
+                System.out.println("Este numero esta contido no vetor.");
+                break;
+            }else if(i == tamanho -1 && vetor[1][0] != vetor[0][i]){
+                System.out.println("Numero nao encontrado no vetor.");
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Uni6Exe06 - implementar solução");
+        Uni6Exe06 init = new Uni6Exe06();
+        init.entrada();
+        init.popularVetor();
+        init.checarValor();
     }
 }
