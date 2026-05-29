4fe380b2615700fd28d0080a03e3adaef580893c
Alinor <havengar@hotmail.com.br>
2026-05-29 18:01:48 -0300

    Add Uni5Exe24: Implement fishing championship logic with structured input handling for improved readability and maintainability

--- src/Unidade_5/Uni5Exe24.java
+++ src/Unidade_5/Uni5Exe24.java
@@ -4,22 +4,25 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni5Exe24 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("0.00");
-        DecimalFormat cm = new DecimalFormat("0");
-        boolean exit = true;
-        char comfirma;
-        float limiteDiario = 150;
-        float pescou = 0;
-        float peso = 0;
-        float pegos = 0;
-        float l = 0;
-        float w = 0;
-        float a = 0.0278f;
-        float b = 2.79f;
-        float v = 0;
+    private Scanner sc = new Scanner(System.in);
+    private DecimalFormat df = new DecimalFormat("0.00");
+    private DecimalFormat cm = new DecimalFormat("0");
+    private boolean exit = true;
+    private char comfirma;
+    private float limiteDiario = 150;
+    private float pescou = 0;
+    private float peso = 0;
+    private float pegos = 0;
+    private float l = 0;
+    private float w = 0;
+    private float a = 0.0278f;
+    private float b = 2.79f;
+    private float v = 0;
 
+    public Uni5Exe24(){
+        logica();
+    }
+    private void logica(){
         while (exit) {
             System.out.println("Campeonato de pesca de pirarucu!\nLimite diario de "+df.format(limiteDiario)+" kg");
             if(limiteDiario <= peso){
@@ -38,7 +41,47 @@ public class Uni5Exe24 {
                 }
             }
         }
+        imprimir();
+    }
+    private void imprimir(){
         System.out.println("Você pegou um total de "+cm.format(pegos)+" peixes.\nCom o total de "+df.format(peso)+" kg");
-        sc.close();
+    }
+    public static void main(String[] args) {
+        new Uni5Exe24();
+    //     Scanner sc = new Scanner(System.in);
+    //     DecimalFormat df = new DecimalFormat("0.00");
+    //     DecimalFormat cm = new DecimalFormat("0");
+    //     boolean exit = true;
+    //     char comfirma;
+    //     float limiteDiario = 150;
+    //     float pescou = 0;
+    //     float peso = 0;
+    //     float pegos = 0;
+    //     float l = 0;
+    //     float w = 0;
+    //     float a = 0.0278f;
+    //     float b = 2.79f;
+    //     float v = 0;
+
+    //     while (exit) {
+    //         System.out.println("Campeonato de pesca de pirarucu!\nLimite diario de "+df.format(limiteDiario)+" kg");
+    //         if(limiteDiario <= peso){
+    //             exit = false;
+    //         }else{
+    //             l = (int)(Math.random() * ((270 - 50) + 1) + 50);
+    //             w = ((int)Math.pow(l, b)) * a;
+    //             v = (int)(Math.random() * ((15f - 0) + 1));
+    //             v = v / 100;
+    //             pescou = (w + (w * v)) / 1000;
+    //             System.out.print("Você pegou um pirarucu!\n   Tamanho: "+cm.format(l)+"cm\n   Peso: "+df.format(pescou)+" kg\nDeseja capturar? S(SIM)/N(NÃO): ");
+    //             comfirma = sc.next().toLowerCase().charAt(0);
+    //             if(comfirma == 's'){
+    //                 peso += pescou;
+    //                 pegos++;
+    //             }
+    //         }
+    //     }
+    //     System.out.println("Você pegou um total de "+cm.format(pegos)+" peixes.\nCom o total de "+df.format(peso)+" kg");
+    //     sc.close();
     }
 }
