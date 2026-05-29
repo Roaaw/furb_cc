fdfb930bb1994bf4500efe99fc5055d36a639115
Alinor <havengar@hotmail.com.br>
2026-05-29 16:16:04 -0300

    Refactor Uni5Exe15: Streamline input handling and improve logic flow for student grades

--- src/Unidade_5/Uni5Exe15.java
+++ src/Unidade_5/Uni5Exe15.java
@@ -9,57 +9,66 @@ public class Uni5Exe15 {
     private boolean exit = false;
 
     public Uni5Exe15(){
-
+        entrada();
+        logica();
     }
     private void entrada(){
         if(nome == ""){
             System.out.print("Nome do aluno: ");
             nome = sc.next();
-        }
-        if(nota == 0){
+        }else if(nota == 0){
             System.out.print("Digite a nota: ");
             nota = sc.nextFloat();
-        }else{
             System.out.print("Digite a próxima nota: ");
             nota += sc.nextFloat();
         }
     }
     private void logica(){
         while (!exit) {
+            if(nome == ""){
+                entrada();
+            }
             if(nome.toLowerCase().equals("fim")){
                 exit = true;
+                break;
             }else{
-                for(int i=0; i <=1; i++){
-                    
-                }
+                entrada();
+                imprimir();
+                nota = 0;
+                nome = "";
             }
         }
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float nota = 0;
-        String nome = "";
-        boolean exit = true;
-        while (exit) {
-            System.out.print("Nome do aluno: ");
-            nome = sc.next();
-            if(nome.toLowerCase().equals("fim")){
-                exit = false;
-            }else{
-                for(int i=0; i <=1; i++){
-                    if(nota == 0){
-                        System.out.print("Digite a nota: ");
-                        nota = sc.nextFloat();
-                    }else{
-                        System.out.print("Digite a próxima nota: ");
-                        nota += sc.nextFloat();
-                    }
-                }
-                System.out.println("A médua do(a) "+nome+" é "+(nota/2));
-                nota = 0;
-                }
-        }
+    private void imprimir(){
+        System.out.println("A média do(a) "+nome+" é "+(nota/2));
         System.out.println("-");
-        sc.close();
+    }
+    public static void main(String[] args) {
+        new Uni5Exe15();
+        // Scanner sc = new Scanner(System.in);
+        // float nota = 0;
+        // String nome = "";
+        // boolean exit = true;
+        // while (exit) {
+        //     System.out.print("Nome do aluno: ");
+        //     nome = sc.next();
+        //     if(nome.toLowerCase().equals("fim")){
+        //         exit = false;
+        //     }else{
+        //         for(int i=0; i <=1; i++){
+        //             if(nota == 0){
+        //                 System.out.print("Digite a nota: ");
+        //                 nota = sc.nextFloat();
+        //             }else{
+        //                 System.out.print("Digite a próxima nota: ");
+        //                 nota += sc.nextFloat();
+        //             }
+        //         }
+        //         System.out.println("A médua do(a) "+nome+" é "+(nota/2));
+        //         nota = 0;
+        //         }
+        // }
+        // System.out.println("-");
+        // sc.close();
     }
 }
