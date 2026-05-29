820f58bbe16bd56068e19492da8932801d6f3166
Alinor <havengar@hotmail.com.br>
2026-05-29 17:39:56 -0300

    Refactor Uni5Exe19: Encapsulate input handling and logic into methods for improved readability and maintainability

--- src/Unidade_5/Uni5Exe19.java
+++ src/Unidade_5/Uni5Exe19.java
@@ -4,16 +4,23 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni5Exe19 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("0.00");
-        boolean exit = true;
-        float desconto = 0;
-        float valorCompra = 0;
-        float soma = 0;
+    private Scanner sc = new Scanner(System.in);
+    private DecimalFormat df = new DecimalFormat("0.00");
+    private boolean exit = true;
+    private float desconto = 0;
+    private float valorCompra = 0;
+    private float soma = 0;
+
+    public Uni5Exe19(){
+        logica();
+    }
+    private void entrada(){
+        System.out.print("Valor da compra: ");
+        valorCompra = sc.nextFloat();
+    }
+    private void logica(){
         while (exit) {
-            System.out.print("Valor da compra: ");
-            valorCompra = sc.nextFloat();
+            entrada();
             if(valorCompra == 0){
                 exit = false;
             }
@@ -26,10 +33,45 @@ public class Uni5Exe19 {
             }
             soma += valorCompra;
             if(valorCompra != 0){
-                System.out.println("Valor a pagar: R$"+df.format(valorCompra));
+                imprimir();
             }
         }
-        System.out.println("O valor total recebido foi de R$"+df.format(soma));
-        sc.close();
+        imprimir();
+    }
+    private void imprimir(){
+        if(valorCompra != 0){
+            System.out.println("Valor a pagar: R$"+df.format(valorCompra));
+        }else{
+            System.out.println("O valor total recebido foi de R$"+df.format(soma));
+        }
+    }
+    public static void main(String[] args) {
+        new Uni5Exe19();
+        // Scanner sc = new Scanner(System.in);
+        // DecimalFormat df = new DecimalFormat("0.00");
+        // boolean exit = true;
+        // float desconto = 0;
+        // float valorCompra = 0;
+        // float soma = 0;
+        // while (exit) {
+        //     System.out.print("Valor da compra: ");
+        //     valorCompra = sc.nextFloat();
+        //     if(valorCompra == 0){
+        //         exit = false;
+        //     }
+        //     if(valorCompra > 500){
+        //         desconto = 0.20f;
+        //         valorCompra = valorCompra - (valorCompra * desconto);
+        //     }else{
+        //         desconto = 0.15f;
+        //         valorCompra = valorCompra - (valorCompra * desconto);
+        //     }
+        //     soma += valorCompra;
+        //     if(valorCompra != 0){
+        //         System.out.println("Valor a pagar: R$"+df.format(valorCompra));
+        //     }
+        // }
+        // System.out.println("O valor total recebido foi de R$"+df.format(soma));
+        // sc.close();
     }
 }
