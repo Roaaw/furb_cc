d2d3a10c0a34e977e25bbfe09351c8f1580968c0
Alinor <havengar@hotmail.com.br>
2026-05-29 17:44:49 -0300

    Refactor Uni5Exe20: Encapsulate logic and input handling into methods for improved readability and maintainability

--- src/Unidade_5/Uni5Exe20.java
+++ src/Unidade_5/Uni5Exe20.java
@@ -4,18 +4,25 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni5Exe20 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("0.00");
-        DecimalFormat grms = new DecimalFormat("0.000");
-        DecimalFormat clk = new DecimalFormat("0");
-        float tempo = 0;
-        float massa = 0;
-        float initmassa;
-        boolean exit = true;
-        String text = "";
+    private Scanner sc = new Scanner(System.in);
+    private DecimalFormat df = new DecimalFormat("0.00");
+    private DecimalFormat grms = new DecimalFormat("0.000");
+    private DecimalFormat clk = new DecimalFormat("0");
+    private float tempo = 0;
+    private float massa = 0;
+    private float initmassa;
+    private boolean exit = true;
+    private String text = "";
+
+    public Uni5Exe20(){
+        logica();
+    }
+    private void entrada(){
         System.out.print("Insira a massa inicial em Kg: ");
         massa = sc.nextFloat();
+    }
+    private void logica(){
+        entrada();
         initmassa = massa;
         massa *= 1000;
         while (exit) {
@@ -27,16 +34,54 @@ public class Uni5Exe20 {
                 tempo+= 50;
                 text += "\nMassa final: "+grms.format(massa)+" gramas";
                 if(tempo % 60 != 0){
-                    text += "\nTempo: "+clk.format(tempo / 60)+" min"+clk.format(tempo % 60)+" seg";
+                    text += "\nTempo: "+clk.format(tempo / 60)+" min "+clk.format(tempo % 60)+" seg";
                 }else{
                     text+= "\nTempo: "+clk.format(tempo / 60)+" min";
                 }
-                System.out.println(tempo);
+                // System.out.println(tempo);
                 exit = false; 
             }
             tempo+= 50;
         }
-        sc.close();
+        imprimir();
+    }
+    private void imprimir(){
         System.out.println(text);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe20();
+        // Scanner sc = new Scanner(System.in);
+        // DecimalFormat df = new DecimalFormat("0.00");
+        // DecimalFormat grms = new DecimalFormat("0.000");
+        // DecimalFormat clk = new DecimalFormat("0");
+        // float tempo = 0;
+        // float massa = 0;
+        // float initmassa;
+        // boolean exit = true;
+        // String text = "";
+        // System.out.print("Insira a massa inicial em Kg: ");
+        // massa = sc.nextFloat();
+        // initmassa = massa;
+        // massa *= 1000;
+        // while (exit) {
+        //     if(massa == initmassa){
+        //         text += "Massa inicial: "+df.format(massa)+"Kg";
+        //     }
+        //     massa /= 2;
+        //     if(massa <= 0.5f){
+        //         tempo+= 50;
+        //         text += "\nMassa final: "+grms.format(massa)+" gramas";
+        //         if(tempo % 60 != 0){
+        //             text += "\nTempo: "+clk.format(tempo / 60)+" min"+clk.format(tempo % 60)+" seg";
+        //         }else{
+        //             text+= "\nTempo: "+clk.format(tempo / 60)+" min";
+        //         }
+        //         System.out.println(tempo);
+        //         exit = false; 
+        //     }
+        //     tempo+= 50;
+        // }
+        // sc.close();
+        // System.out.println(text);
     }
 }
