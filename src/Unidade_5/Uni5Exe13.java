dc75630bf34659fdef999b255893b299d6133300
Alinor <havengar@hotmail.com.br>
2026-05-29 15:54:10 -0300

    Refactor Uni5Exe13: Enhance data handling by adding cumulative calculations and final output for average mileage

--- src/Unidade_5/Uni5Exe13.java
+++ src/Unidade_5/Uni5Exe13.java
@@ -1,11 +1,14 @@
 package Unidade_5;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni5Exe13 {
     private Scanner sc = new Scanner(System.in);
+    private DecimalFormat df = new DecimalFormat("0.00");
     private int n = 0;
-    private float km,c;
+    private float km,c,kma,ca;
+    private boolean fimLoop = false;
     
     public Uni5Exe13(){
         logica();
@@ -17,13 +20,18 @@ public class Uni5Exe13 {
     private void entradaDados(){
         System.out.print("Insira a Quilometragem do Odometro: ");
         km = sc.nextFloat();
+        kma += km;
         System.out.print("Insira a quantidade de litros da comanda de abastecimento: ");
         c = sc.nextFloat();
+        ca += c;
     }
     private void logica(){
         entrada();
         for(int i=0; i<=(n-1); i++){
             entradaDados();
+            if(i == (n-1)){
+                fimLoop = true;
+            }
             imprimir(i);
         }
     }
@@ -31,6 +39,9 @@ public class Uni5Exe13 {
         System.out.printf("Parada %s: Quilometragem: %s km, Combustivel: %s litros\n",i+1,km,c);
         km = km/c;
         System.out.printf("Parada %s: %s km por litro\n",i+1,km);
+        if (fimLoop == true){
+            System.out.println("Quilometragem média obtida por litro: "+df.format((kma/ca)));
+        }
     }
     public static void main(String[] args) {
         new Uni5Exe13();
