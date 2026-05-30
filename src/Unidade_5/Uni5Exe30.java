e4844686e5b115c451301e4313e415def29f845c
Alinor <havengar@hotmail.com.br>
2026-05-29 22:08:47 -0300

    Add Uni5Exe30 and Uni5Exe31: Refactor input handling and logic into methods for improved readability and maintainability

--- src/Unidade_5/Uni5Exe30.java
+++ src/Unidade_5/Uni5Exe30.java
@@ -3,24 +3,25 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe30 {
+    private Scanner sc = new Scanner(System.in);
+    private boolean exit = false;
+    private int n,k;
+    private int m = 10;
+    private int somaentra = 0;
+    private int somafora = 0;
+    private String entrou = "";
+    private String fora = "";
+    private String aentrar = "";
+
     private Uni5Exe30(){
-        
+        logica();
     }
     private void entrada(){
-        
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean exit = false;
-        int n,k;
-        int m = 10;
-        int somaentra = 0;
-        int somafora = 0;
-        String entrou = "";
-        String fora = "";
-        String aentrar = "";
         n = sc.nextInt();
         k = sc.nextInt();
+    }
+    private void logica(){
+        entrada();
         while(!exit){
             if(m - n >= 0 && n > 0){
                 m -= n;
@@ -54,7 +55,58 @@ public class Uni5Exe30 {
             }
             
         }
+        imprimir();
+    }
+    private void imprimir(){
         System.out.println("Elementos que entraram na mochila: "+entrou+"\nElementos que ficaram de fora na mochila: "+fora+"\nElementos a entrar mochila: "+aentrar+"\nSoma dos elementos que entraram na mochila: "+somaentra+"\nSoma dos elementos que ficaram de fora na mochila: "+somafora);
-        sc.close();
+    }
+    public static void main(String[] args) {
+        new Uni5Exe30();
+        // Scanner sc = new Scanner(System.in);
+        // boolean exit = false;
+        // int n,k;
+        // int m = 10;
+        // int somaentra = 0;
+        // int somafora = 0;
+        // String entrou = "";
+        // String fora = "";
+        // String aentrar = "";
+        // n = sc.nextInt();
+        // k = sc.nextInt();
+        // while(!exit){
+        //     if(m - n >= 0 && n > 0){
+        //         m -= n;
+        //         if(entrou.equals("")){
+        //             entrou += n;
+        //         }else{
+        //             entrou += ","+n;
+        //         }
+        //         somaentra += n;
+        //         if(aentrar.equals("")){
+        //             aentrar += n;
+        //         }else{
+        //             aentrar += ","+n;
+        //         }
+        //         n -= k;
+        //     }else if (m == 0 && n > 0){
+        //         if(fora.equals("")){
+        //             fora += n;
+        //         }else{
+        //             fora += ","+n;
+        //         }
+        //         if(aentrar.equals("")){
+        //             aentrar += n;
+        //         }else{
+        //             aentrar += ","+n;
+        //         }
+        //         somafora += n;
+        //         n -= k;
+        //     }else{
+        //         exit = true;
+        //     }
+            
+        // }
+        // System.out.println("Elementos que entraram na mochila: "+entrou+"\nElementos que ficaram de fora na mochila: "+fora+"\nElementos a entrar mochila: "+aentrar+"\nSoma dos elementos que entraram na mochila: "+somaentra+"\nSoma dos elementos que ficaram de fora na mochila: "+somafora);
+        // sc.close();
     }
 }
--- src/Unidade_5/Uni5Exe31.java
+++ src/Unidade_5/Uni5Exe31.java
@@ -3,13 +3,20 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe31 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um número: ");
-        int num = sc.nextInt();
-        boolean exit = false;
-        int div = 2;
-        String text = "|Número|Decomposição|";
+    private Scanner sc = new Scanner(System.in);
+    private int num;
+    private boolean exit = false;
+    private int div = 2;
+    private String text = "|Número|Decomposição|";
+    public Uni5Exe31(){
+        logica();
+    }
+    private void entrada(){
+        System.out.print("Digite um número: ");
+        num = sc.nextInt();
+    }
+    private void logica(){
+        entrada();
         while (!exit) {
             if(num % div == 0 && div / 1 == div && div / div == 1){
                 if(num >= 1000 && num < 9999){
@@ -25,7 +32,11 @@ public class Uni5Exe31 {
             }else if(num % div != 0){
                 div++;
             }
-            if(num == 1){
+            imprimir();
+        }
+    }
+    private void imprimir(){
+        if(num == 1){
                 text += "\n|   "+num+"  |"+"            |";
                 exit = true;
                 System.out.println(" ___________________ ");
@@ -36,7 +47,42 @@ public class Uni5Exe31 {
                 System.out.println("Não foi possivel fazer a decomposiçao de fatores primos.");
                 exit = true;
             }
-        }
-        sc.close();
+    }
+    public static void main(String[] args) {
+        new Uni5Exe31();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Digite um número: ");
+        // int num = sc.nextInt();
+        // boolean exit = false;
+        // int div = 2;
+        // String text = "|Número|Decomposição|";
+        // while (!exit) {
+        //     if(num % div == 0 && div / 1 == div && div / div == 1){
+        //         if(num >= 1000 && num < 9999){
+        //             text += "\n| "+num+" |      "+div+"     |";
+        //         }else if(num >=100 && num < 1000){
+        //             text += "\n| "+num+"  |     "+div+"      |";
+        //         }else if(num > 9 && num < 100){
+        //             text += "\n|  "+num+"  |     "+div+"      |";
+        //         }else{
+        //             text += "\n|   "+num+"  |     "+div+"      |";
+        //         }
+        //         num = num / div;
+        //     }else if(num % div != 0){
+        //         div++;
+        //     }
+        //     if(num == 1){
+        //         text += "\n|   "+num+"  |"+"            |";
+        //         exit = true;
+        //         System.out.println(" ___________________ ");
+        //         System.out.println(text);
+        //         System.out.println(" ------------------- ");
+        //     }
+        //     if(num == div && num / div != 1){
+        //         System.out.println("Não foi possivel fazer a decomposiçao de fatores primos.");
+        //         exit = true;
+        //     }
+        // }
+        // sc.close();
     }
 }
