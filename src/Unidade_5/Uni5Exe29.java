193ff0b87989c03789aeac147fa27d19050c3cbc
Alinor <havengar@hotmail.com.br>
2026-05-29 21:14:48 -0300

    Add Uni5Exe29: Implement logic for currency denomination calculation with structured input handling for improved readability and maintainability

--- src/Unidade_5/Uni5Exe29.java
+++ src/Unidade_5/Uni5Exe29.java
@@ -3,12 +3,20 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe29 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean exit = true;
+    private Scanner sc = new Scanner(System.in);
+    private boolean exit = true;
+    private int valor;
+    private String saida = "";
+
+    public Uni5Exe29(){
+        logica();
+    }
+    private void entrada(){
         System.out.print("Informe o valor em R$: ");
-        int valor = sc.nextInt();
-        String saida = "";
+        valor = sc.nextInt();
+    }
+    private void logica(){
+        entrada();
         while (exit) {
             if (valor / 20 != 0){
                 saida += "Notas de R$ 20: "+valor / 20;
@@ -30,7 +38,41 @@ public class Uni5Exe29 {
                 exit = false;
             }
         }
+        imprimir();
+    }
+    private void imprimir(){
         System.out.println(saida);
-        sc.close();
+    }
+
+    public static void main(String[] args) {
+        new Uni5Exe29();
+        // Scanner sc = new Scanner(System.in);
+        // boolean exit = true;
+        // System.out.print("Informe o valor em R$: ");
+        // int valor = sc.nextInt();
+        // String saida = "";
+        // while (exit) {
+        //     if (valor / 20 != 0){
+        //         saida += "Notas de R$ 20: "+valor / 20;
+        //         valor = valor - ((valor / 20) * 20);
+        //     }else if(valor / 10 != 0){
+        //         saida += "\nNotas de R$ 10: "+valor / 10;
+        //         valor = valor - ((valor / 10) * 10);
+        //     }else if(valor / 5 != 0){
+        //         saida += "\nNotas de R$ 5: "+valor / 5;
+        //         valor = valor - ((valor / 5) * 5);
+        //     }else if(valor / 2 != 0){
+        //         saida += "\nNotas de R$ 2: "+valor / 2;
+        //         valor = valor - ((valor / 2) * 2);
+        //     }else if(valor / 1 != 0){
+        //         saida += "\nNotas de R$ 1: "+valor / 1;
+        //         valor = valor - ((valor / 1) * 1);
+        //     }
+        //     if (valor == 0){
+        //         exit = false;
+        //     }
+        // }
+        // System.out.println(saida);
+        // sc.close();
     }
 }
