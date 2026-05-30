4b3e9ef1d1d1bdf9bb9194da337f748cc388250d
Alinor <havengar@hotmail.com.br>
2026-05-29 22:22:01 -0300

    Add Uni5Exe34: Refactor logic into methods for improved readability and maintainability

--- src/Unidade_5/Uni5Exe34.java
+++ src/Unidade_5/Uni5Exe34.java
@@ -4,19 +4,26 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni5Exe34 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("0.00");
-        boolean exit = false;
-        int menu = 0;
-        String nomeH = "";
-        int numDiarias = 0;
-        float calculo = 0;
-        String text = "";
-        int contador = 0;
+    private Scanner sc = new Scanner(System.in);
+    private DecimalFormat df = new DecimalFormat("0.00");
+    private boolean exit = false;
+    private int menu = 0;
+    private String nomeH = "";
+    private int numDiarias = 0;
+    private float calculo = 0;
+    private String text = "";
+    private int contador = 0;
+
+    public Uni5Exe34(){
+        logica();
+    }
+    private void entrada(){
+        System.out.println("1- Encerrar a conta de um hóspede\n2- Verificar número de contas encerradas\n3- Sair");
+        menu = sc.nextInt();
+    }
+    private void logica(){
         do {
-            System.out.println("1- Encerrar a conta de um hóspede\n2- Verificar número de contas encerradas\n3- Sair");
-            menu = sc.nextInt();
+            entrada();
             switch (menu) {
                 case 1:
                     System.out.print("Informe o nome do hóspede: ");
@@ -25,20 +32,10 @@ public class Uni5Exe34 {
                     System.out.print("Informe o número de diárias: ");
                     numDiarias = sc.nextInt();
                     contador++;
-                    if(numDiarias < 15){
-                        calculo = (50 + 7.50f) * numDiarias;
-                        text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
-                    }else if(numDiarias == 15){
-                        calculo = (50 + 6.50f) * numDiarias;
-                        text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
-                    }else if(numDiarias > 15){
-                        calculo = (50 + 5.00f) * numDiarias;
-                        text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
-                    }
-                    System.out.println(text);
+                    imprimirEncerrarConta();
                     break;
                 case 2:
-                    System.out.println("Número de contas encerradas: "+contador);
+                    imprimirVerificar();
                     break;
                 case 3:
                     exit = true;
@@ -48,6 +45,68 @@ public class Uni5Exe34 {
                     break;
             }
         } while (!exit);
-        sc.close();
+    }
+    private void imprimirEncerrarConta(){
+        if(numDiarias < 15){
+            calculo = (50 + 7.50f) * numDiarias;
+            text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
+        }else if(numDiarias == 15){
+            calculo = (50 + 6.50f) * numDiarias;
+            text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
+        }else if(numDiarias > 15){
+            calculo = (50 + 5.00f) * numDiarias;
+            text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
+        }
+        System.out.println(text);
+    }
+    private void imprimirVerificar(){
+        System.out.println("Número de contas encerradas: "+contador);
+    }
+    public static void main(String[] args) {
+        new Uni5Exe34();
+        // Scanner sc = new Scanner(System.in);
+        // DecimalFormat df = new DecimalFormat("0.00");
+        // boolean exit = false;
+        // int menu = 0;
+        // String nomeH = "";
+        // int numDiarias = 0;
+        // float calculo = 0;
+        // String text = "";
+        // int contador = 0;
+        // do {
+        //     System.out.println("1- Encerrar a conta de um hóspede\n2- Verificar número de contas encerradas\n3- Sair");
+        //     menu = sc.nextInt();
+        //     switch (menu) {
+        //         case 1:
+        //             System.out.print("Informe o nome do hóspede: ");
+        //             nomeH = sc.next();
+        //             sc.nextLine();
+        //             System.out.print("Informe o número de diárias: ");
+        //             numDiarias = sc.nextInt();
+        //             contador++;
+        //             if(numDiarias < 15){
+        //                 calculo = (50 + 7.50f) * numDiarias;
+        //                 text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
+        //             }else if(numDiarias == 15){
+        //                 calculo = (50 + 6.50f) * numDiarias;
+        //                 text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
+        //             }else if(numDiarias > 15){
+        //                 calculo = (50 + 5.00f) * numDiarias;
+        //                 text = "Nome: "+nomeH+" total a pagar: R$"+df.format(calculo)+"\n";
+        //             }
+        //             System.out.println(text);
+        //             break;
+        //         case 2:
+        //             System.out.println("Número de contas encerradas: "+contador);
+        //             break;
+        //         case 3:
+        //             exit = true;
+        //             break;
+            
+        //         default:
+        //             break;
+        //     }
+        // } while (!exit);
+        // sc.close();
     }
 }
