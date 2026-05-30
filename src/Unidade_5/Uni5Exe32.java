32731ef4b06aac86907ea75b356c51da6cab54b0
Alinor <havengar@hotmail.com.br>
2026-05-29 22:46:20 -0300

    Add Uni5Exe32: Implement calendar logic with user input for day and month

--- src/Unidade_5/Uni5Exe32.java
+++ src/Unidade_5/Uni5Exe32.java
@@ -3,7 +3,48 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe32 {
+    private Scanner sc = new Scanner(System.in);
+    private int d = 0;
+    private int m = 0;
+
+    public Uni5Exe32(){
+        logica();
+    }
+    private void entrada(){
+        System.out.print("Digite o dia da semana que cai o primeiro dia: ");
+        d = sc.nextInt();
+        System.out.print("Digite a quantidade de dias no mês: ");
+        m = sc.nextInt();
+    }
+    private void logica(){
+        entrada();
+        System.out.println("Dom    Seg    Ter    Qua    Qui    Sex    Sab");
+        for(int i = 1; i < d; i++){
+            System.out.print("       ");
+        }
+        for(int i = 1; i <= m; i++){
+            System.out.printf("%-7d", i);
+            if((d - 1 + i) % 7 == 0){
+                System.out.println();
+            }
+        }
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        new Uni5Exe32();
+        // Scanner sc = new Scanner(System.in);
+        // int d = 0;
+        // int m = 0;
+        // d = sc.nextInt();
+        // m = sc.nextInt();
+        // System.out.println("Dom    Seg    Ter    Qua    Qui    Sex    Sab");
+        // for(int i = 1; i < d; i++){
+        //     System.out.print("       ");
+        // }
+        // for(int i = 1; i <= m; i++){
+        //     System.out.printf("%-7d", i);
+        //     if((d - 1 + i) % 7 == 0){
+        //         System.out.println();
+        //     }
+        // }
     }
 }
