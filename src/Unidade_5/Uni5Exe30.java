8b0e6e812ac716328e4d97f2fd2d353d1c6a0870
Alinor <havengar@hotmail.com.br>
2026-05-29 21:31:24 -0300

    .

--- src/Unidade_5/Uni5Exe30.java
+++ src/Unidade_5/Uni5Exe30.java
@@ -3,6 +3,12 @@ package Unidade_5;
 import java.util.Scanner;
 
 public class Uni5Exe30 {
+    private Uni5Exe30(){
+        
+    }
+    private void entrada(){
+        
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean exit = false;
