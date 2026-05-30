e76fa42713d1b1f46be9c9f817fe447fd5af9501
Alinor <havengar@hotmail.com.br>
2026-05-29 23:02:12 -0300

    Refactor Uni5Exe30: Improve variable naming and streamline logic for clarity and maintainability

--- src/Unidade_5/Uni5Exe30.java
+++ src/Unidade_5/Uni5Exe30.java
@@ -4,109 +4,66 @@ import java.util.Scanner;
 
 public class Uni5Exe30 {
     private Scanner sc = new Scanner(System.in);
-    private boolean exit = false;
     private int n,k;
     private int m = 10;
-    private int somaentra = 0;
-    private int somafora = 0;
+    private int somaEntra = 0;
+    private int somaFora = 0;
     private String entrou = "";
     private String fora = "";
-    private String aentrar = "";
+    private String aEntrar = "";
 
     private Uni5Exe30(){
         logica();
     }
     private void entrada(){
+        System.out.print("");
         n = sc.nextInt();
+        System.out.print("");
         k = sc.nextInt();
     }
     private void logica(){
         entrada();
-        while(!exit){
-            if(m - n >= 0 && n > 0){
-                m -= n;
-                if(entrou.equals("")){
-                    entrou += n;
-                }else{
-                    entrou += ","+n;
-                }
-                somaentra += n;
-                if(aentrar.equals("")){
-                    aentrar += n;
-                }else{
-                    aentrar += ","+n;
-                }
-                n -= k;
-            }else if (m == 0 && n > 0){
-                if(fora.equals("")){
-                    fora += n;
-                }else{
-                    fora += ","+n;
-                }
-                if(aentrar.equals("")){
-                    aentrar += n;
-                }else{
-                    aentrar += ","+n;
-                }
-                somafora += n;
-                n -= k;
+        int valorAtual = n;
+        int capacidadeRestante = m;
+        int temp = n;
+        while(temp > 0){
+            if(aEntrar.equals("")){
+                aEntrar += temp;
             }else{
-                exit = true;
+                aEntrar += "," + temp;
             }
-            
+            temp -= k;
+        }
+        while (valorAtual > 0) {
+            if(capacidadeRestante - valorAtual >= 0){
+                capacidadeRestante -= valorAtual;
+                if(entrou.equals("")){
+                    entrou += valorAtual;
+                }else{
+                    entrou += "," + valorAtual;
+                }
+                somaEntra += valorAtual;
+            }else{
+                if(fora.equals("")){
+                    fora += valorAtual;
+                }else{
+                    fora += "," + valorAtual;
+                }
+                somaFora += valorAtual;
+            }
+            valorAtual -= k;
         }
         imprimir();
     }
     private void imprimir(){
-        System.out.println("Elementos que entraram na mochila: "+entrou+"\nElementos que ficaram de fora na mochila: "+fora+"\nElementos a entrar mochila: "+aentrar+"\nSoma dos elementos que entraram na mochila: "+somaentra+"\nSoma dos elementos que ficaram de fora na mochila: "+somafora);
+        System.out.println("Elementos a serem colocados na mochila (sequência): " + aEntrar);
+        System.out.println("Elementos que entraram na mochila: " + entrou);
+        System.out.println("Elementos que ficaram fora da mochila: " + fora);
+        System.out.println("Soma dos elementos que entraram: " + somaEntra);
+        System.out.println("Soma dos elementos que ficaram fora: " + somaFora);
+        // System.out.println("Elementos que entraram na mochila: "+aentrar+"\nElementos que ficaram de fora na mochila: "+fora+"\nElementos a entrar mochila: "+entrou+"\nSoma dos elementos que entraram na mochila: "+somaentra+"\nSoma dos elementos que ficaram de fora na mochila: "+somafora);
     }
     public static void main(String[] args) {
         new Uni5Exe30();
-        // Scanner sc = new Scanner(System.in);
-        // boolean exit = false;
-        // int n,k;
-        // int m = 10;
-        // int somaentra = 0;
-        // int somafora = 0;
-        // String entrou = "";
-        // String fora = "";
-        // String aentrar = "";
-        // n = sc.nextInt();
-        // k = sc.nextInt();
-        // while(!exit){
-        //     if(m - n >= 0 && n > 0){
-        //         m -= n;
-        //         if(entrou.equals("")){
-        //             entrou += n;
-        //         }else{
-        //             entrou += ","+n;
-        //         }
-        //         somaentra += n;
-        //         if(aentrar.equals("")){
-        //             aentrar += n;
-        //         }else{
-        //             aentrar += ","+n;
-        //         }
-        //         n -= k;
-        //     }else if (m == 0 && n > 0){
-        //         if(fora.equals("")){
-        //             fora += n;
-        //         }else{
-        //             fora += ","+n;
-        //         }
-        //         if(aentrar.equals("")){
-        //             aentrar += n;
-        //         }else{
-        //             aentrar += ","+n;
-        //         }
-        //         somafora += n;
-        //         n -= k;
-        //     }else{
-        //         exit = true;
-        //     }
-            
-        // }
-        // System.out.println("Elementos que entraram na mochila: "+entrou+"\nElementos que ficaram de fora na mochila: "+fora+"\nElementos a entrar mochila: "+aentrar+"\nSoma dos elementos que entraram na mochila: "+somaentra+"\nSoma dos elementos que ficaram de fora na mochila: "+somafora);
-        // sc.close();
     }
 }
