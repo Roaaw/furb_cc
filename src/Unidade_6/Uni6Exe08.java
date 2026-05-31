788c46a8b100eeaec21e961e08a7430d7c764ce0
Alinor <havengar@hotmail.com.br>
2026-05-31 20:32:07 -0300

    Add Uni6Exe08: Implement frequency calculation for array values and enhance output formatting

--- src/Unidade_6/Uni6Exe08.java
+++ src/Unidade_6/Uni6Exe08.java
@@ -1,12 +1,14 @@
 package Unidade_6;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni6Exe08 {
     private Scanner sc = new Scanner(System.in);
-    private int tamanho; //<= 20
-    private double[] valor;
-    private int contador = 1;
+    private DecimalFormat df = new DecimalFormat("0.00");
+    private int tamanho = 9; //<= 20
+    private double[] valor = new double[9];
+    private double[][] freq = new double[2][9];
 
     public Uni6Exe08(){
         tamVetor();
@@ -28,22 +30,53 @@ public class Uni6Exe08 {
             System.out.print("Insira os valores: ");
             valor[i] = sc.nextDouble();
         }
+        // valor[0] = 2.1f;
+        // valor[1] = 3.7f;
+        // valor[2] = -4.2f;
+        // valor[3] = 2.1f;
+        // valor[4] = 3.75f;
+        // valor[5] = -4.2f;
+        // valor[6] =  2.1f;
+        // valor[7] = 7f;
+        // valor[8] = 7f;
     }
     private void checarVetor(){
         for (int i = 0; i < tamanho; i++){
             if(i+1 >= tamanho){
                 break;
             }else{
-                for (int j = i+1; j < tamanho; j++){
-                    if(valor[i] == valor[j]){
-                        contador ++;
+                for (int j = 0; j < tamanho; j++){
+                    if(valor[i] == freq[0][j]){
+                        break;
+                    }else if(j == tamanho-1){
+                        freq[0][i] = valor[i];
+                        if(freq[1][i] == 0){
+                            freq[1][i] = 1;
+                        }
+                        for (int k = i+1; k < tamanho; k++){
+                            if(freq[0][i] == valor[k]){
+                                freq[1][i] += 1; 
+                            }
+                        }
                     }
                 }
             }
         }
+        System.out.println("Valor"+" | "+"Frequencia");
+        for (int i = 0; i < tamanho; i++){
+            if(freq[0][i] != 0){
+                if(freq[0][i] > 0){
+                    System.out.printf(" "+df.format(freq[0][i])+" | %.0f",freq[1][i]);
+                    System.out.println();
+                }else{
+                    System.out.printf(df.format(freq[0][i])+" | %.0f",freq[1][i]);
+                    System.out.println();
+                }
+                
+            }
+        }
     }
     public static void main(String[] args) {
-        System.out.println("Uni6Exe08 - implementar solução");
         new Uni6Exe08();
     }
 }
