b54afdb99ed74935b7c5c8c7555091e0b116b351
Alinor <havengar@hotmail.com.br>
2026-06-04 16:41:12 -0300

    Refactor Uni6Exe09: Enhance input validation and add median calculations for gender and age demographics

--- src/Unidade_6/Uni6Exe09.java
+++ src/Unidade_6/Uni6Exe09.java
@@ -1,18 +1,20 @@
 package Unidade_6;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni6Exe09 {
     private Scanner sc = new Scanner(System.in);
+    private DecimalFormat df = new DecimalFormat("0.00");
     private int[] sexo;
     private double[] nota;
     private int[] idade;
     private int tamVetor;
     private int indiceVetor = 0;
     private int opcaoMenu = 0;
-    private double media = 0;
+    private double[] media = new double[2];
     private int contador = 0;
-    private int temp = 0;
+    private int temp = Integer.MAX_VALUE;
 
     public Uni6Exe09(){
         tamanhoVetor();
@@ -26,6 +28,9 @@ public class Uni6Exe09 {
                 case 0:
                     System.out.println("Encerrando programa . . .");
                     notaMedia();
+                    notaMediaH();
+                    notaMJoven();
+                    maiorMediaMCPlus();
                     break;
             
                 default:
@@ -36,7 +41,7 @@ public class Uni6Exe09 {
     }
 
     private void tamanhoVetor(){
-        System.out.println("");
+        System.out.printf("Informe o tamanho do vetor: ");
         tamVetor = sc.nextInt();
         sexo = new int[tamVetor];
         nota = new double[tamVetor];
@@ -45,15 +50,40 @@ public class Uni6Exe09 {
 
     private void sexoCliente(){
         System.out.print("\n1-Feminino\n2-Masculino\nInforme o seu sexo: ");
-        sexo[indiceVetor] = sc.nextInt();
+        switch (sc.nextInt()) {
+            case 1:
+                sexo[indiceVetor] = 1;
+                break;
+            case 2:
+                sexo[indiceVetor] = 2;
+                break;
+        
+            default:
+                System.out.println("Escolha uma das opções abaixo.");
+                sexoCliente();
+                break;
+        }
     }
     private void idadeCliente(){
         System.out.print("\nInforme a sua idade: ");
-        idade[indiceVetor] = sc.nextInt();
+        temp = sc.nextInt();
+        if(temp > 0){
+            idade[indiceVetor] = temp;
+        }else{
+            System.out.println("Idade invalida.");
+            idadeCliente();
+        }
     }
     private void notaCinema(){
-        System.out.print("\nInforme nota dada ao cinema: ");
-        nota[indiceVetor] = sc.nextInt();
+        System.out.print("\nInforme nota dada ao cinema(0 a 10): ");
+        temp = sc.nextInt();
+        if(temp > 10 || temp < 0){
+            System.out.println("Erro! Informe uma nota de 0 a 10");
+            notaCinema();
+        }else{
+            nota[indiceVetor] = temp;
+        }
+        
     }
     private void pesquisaCinema(){
         sexoCliente();
@@ -63,24 +93,51 @@ public class Uni6Exe09 {
     }
     private void notaMedia(){
         for(int i = 0; i < indiceVetor; i++){
-            media += nota[i];
+            media[0] += nota[i];
         }
-        media /= indiceVetor;
-        System.out.println("Nota média do cinema: "+media);
+        media[0] /= indiceVetor;
+        System.out.println("Nota média do cinema: "+df.format(media[0]));
     }
     private void notaMediaH(){
-        media = 0;
         for(int i = 0; i < indiceVetor; i++){
-            if (sexo[i] == '2'){
-                media += nota[i];
+            if (sexo[i] == 2){
+                media[1] += nota[i];
                 contador++;
             }
         }
-        media /= contador;
+        media[1] /= contador;
+        System.out.println("Nota média dada apenas por homens: "+df.format(media[1]));
     }
     private void notaMJoven(){
+        temp = Integer.MAX_VALUE;
+        contador = -1;
         for(int i = 0; i < indiceVetor; i++){
-            
+            if(sexo[i] == 1){
+                if (temp > idade[i]){
+                    temp = idade[i];
+                    contador++;
+                }
+            }
+        }
+        if(temp < 0){
+            System.out.println("nenhuma mulher respondeu a votação.");
+        }else{
+            System.out.println("Mulher mais jovem tem "+idade[contador]+" nota foi de "+df.format(nota[contador]));
+        }
+    }
+    private void maiorMediaMCPlus(){
+        contador = 0;
+        temp = 1;
+        for(int i = 0; i < indiceVetor; i++){
+            if(sexo[i] == 1 && idade[i] > 49 && nota[i] >= media[0]){
+                contador++;
+                temp = 0;
+            }
+        }
+        if(temp == 1){
+            System.out.println("Não existe mulheres 50 + que deram nota acima da média.");
+        }else{
+            System.out.println("Mulheres com 50 mais de anos que deram nota acima da média: "+contador);
         }
     }
 
