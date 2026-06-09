625248c776e6a68e706d67350eaa84371f525fbe
Alinor <havengar@hotmail.com.br>
2026-06-09 15:31:53 -0300

    Refactor Uni6Exe10Array: Replace array with ArrayList for improved data handling and clarity

--- src/Unidade_6/Uni6Exe10Array.java
+++ src/Unidade_6/Uni6Exe10Array.java
@@ -1,11 +1,13 @@
 package Unidade_6;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Uni6Exe10Array {
     private Scanner sc = new Scanner(System.in);
     private int opcaoMenu;
-    private int[] vetor = new int[50];
+    // private int[] vetor = new int[50];
+    private ArrayList<Integer> vetor = new ArrayList<>();
     private int posVetor = 0;
     private int entrada = Integer.MIN_VALUE;
     private int indicePesquisa;
@@ -53,10 +55,10 @@ public class Uni6Exe10Array {
     }
 
     private void incluirValor(){
-        if(posVetor < vetor.length){
+        if(50 > vetor.size()){
             System.out.print("Informe o numero a ser incluido no vetor: ");
-            vetor[posVetor] = sc.nextInt();
-            System.out.println("O numero "+vetor[posVetor]+" foi inserido no vetor.");
+            vetor.add(sc.nextInt());
+            System.out.println("O numero "+vetor.get(posVetor)+" foi inserido no vetor.");
             posVetor++;
         }else{
             System.out.println("Vetor está cheio!");
@@ -66,7 +68,7 @@ public class Uni6Exe10Array {
         System.out.print("Informe o valor a ser pesquisado no vetor: ");   
         entrada = sc.nextInt();
         for(int i = 0; i < posVetor; i++){
-            if(entrada == vetor[i]){
+            if(entrada == vetor.get(i)){
                 if(opcaoMenu == 2){
                     System.out.println("O numero "+entrada+" esta na posição "+i+" do vetor.");
                 }
@@ -81,29 +83,29 @@ public class Uni6Exe10Array {
         if(indicePesquisa != -1){
             System.out.println("Informe o numero a ser substituido: ");
             entrada = sc.nextInt();
-            vetor[indicePesquisa] = entrada;
+            vetor.set(indicePesquisa, entrada);
         }
     }
     private void exluirValor(){
         indicePesquisa = pesquisarValor();
         if(indicePesquisa != -1){
             for(int i = indicePesquisa; i < posVetor-1; i++){
-                vetor[i] = vetor[i+1];
+                vetor.remove(i);
             }
             posVetor --;
         }
 
     }
     private void mostrarValores(){
-        if(posVetor == 0 && vetor[posVetor] == 0){
+        if(posVetor == 0 && vetor.get(posVetor) == 0){
             System.out.println("Não existe nada dentro do vetor.");
         }else{
             System.out.print("Os valores armazenados no vetor são: ");
             for(int i = 0; i < posVetor; i++){
                 if(i > 0){
-                    System.out.print(" , "+vetor[i]);
+                    System.out.print(" , "+vetor.get(i));
                 }else{
-                    System.out.print(vetor[i]);
+                    System.out.print(vetor.get(i));
                 }
                 if(i == (posVetor - 1)){
                     System.out.print(".");
@@ -113,23 +115,24 @@ public class Uni6Exe10Array {
         }
     }
     private void ordenarValor(){
-        for(int i = 0 ; i < posVetor; i++){
-            for(int j = 0; j < (posVetor - 1); j++){
-                if(vetor[j] > vetor[i]){
-                    int temp = vetor[i];
-                    vetor[i] = vetor[j];
-                    vetor[j] = temp;
-                }
-            }
-        }
+        vetor.sort(null);
+        // for(int i = 0 ; i < posVetor; i++){
+        //     for(int j = 0; j < (posVetor - 1); j++){
+        //         if(vetor[j] > vetor[i]){
+        //             int temp = vetor[i];
+        //             vetor[i] = vetor[j];
+        //             vetor[j] = temp;
+        //         }
+        //     }
+        // }
 
     }
     private void inverterValor(){
         for(int i = 0 ; i < posVetor; i++){
             for(int j = 0; j < i; j++){
-                int temp = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = temp;
+                int temp = vetor.get(i);
+                vetor.set(i, vetor.get(j));
+                vetor.set(j, temp);
             }
         }
     }
